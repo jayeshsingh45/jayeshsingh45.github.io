fca7d51f2b018c6121ada004e78e93e3e3db18b6
jayeshmax <38346906+jayeshmax@users.noreply.github.com>
2024-04-08 12:52:15 +0530

    all solution

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,6 +1,6 @@
 // NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
-
+// js
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 
